d09345c5a9e1b59bdee8a034a2420df622eb1065
Peter <142713164+KacpeSK@users.noreply.github.com>
2024-02-07 11:57:07 +0100

    move CLARIFAI API call to backend

--- src/App.jsx
+++ src/App.jsx
@@ -8,40 +8,6 @@ import Signin from "./Components/Signin/Signin";
 import Register from "./Components/Register/Register";
 import ParticlesBg from "particles-bg";
 import "./App.css";
-import globalconfig from "./config";
-
-const setupClarifai = (imageUrl) => {
-  const PAT = globalconfig.keys.clarifai.pat;
-  const USER_ID = globalconfig.keys.clarifai.user_id;
-  const APP_ID = globalconfig.keys.clarifai.app_id;
-  const MODEL_ID = "face-detection";
-  const IMAGE_URL = imageUrl;
-  const raw = JSON.stringify({
-    user_app_id: {
-      user_id: USER_ID,
-      app_id: APP_ID,
-    },
-    inputs: [
-      {
-        data: {
-          image: {
-            url: IMAGE_URL,
-          },
-        },
-      },
-    ],
-  });
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      Authorization: "Key " + PAT,
-    },
-    body: raw,
-  };
-
-  return requestOptions;
-};
 
 /*    APP COMPONENT    */
 
@@ -57,8 +23,6 @@ function App() {
     entries: 0,
     joined: "",
   });
-
-  console.log(user);
 
   const onUserChange = (user) => {
     const { id, name, email, entries, joined } = user;
@@ -108,10 +72,13 @@ function App() {
   };
 
   const onButtonSubmit = () => {
-    fetch(
-      "https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs",
-      setupClarifai(imageUrl)
-    )
+    fetch("http://localhost:4001/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        imageUrl: imageUrl,
+      }),
+    })
       .then((response) => response.json())
       .then((result) => {
         if (result) {
